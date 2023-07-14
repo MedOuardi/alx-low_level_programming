fe9743047cdcc7c9b88ce90db58a74715933de6c
Mohammed <ouarrdi@gmail.com>
2023-07-13 19:40:40 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+/*
+ *A C program that prints exactly \"Programming is like building a multilingual puzzle
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
