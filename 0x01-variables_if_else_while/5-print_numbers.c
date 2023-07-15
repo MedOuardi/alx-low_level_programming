e9a7283b6bca7347f056e3b665632c941520bd5a
Mohammed <ouarrdi@gmail.com>
2023-07-14 19:27:40 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print all single numbers
+ *
+ * Return: (0)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (int <= 9)
+	{
+		printf("%i", digit);
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
