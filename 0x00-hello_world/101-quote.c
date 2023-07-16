5ef7d9c4dd33a769f0ff5f0f6a4329c8bef26813
Mohammed <ouarrdi@gmail.com>
2023-07-16 16:09:56 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * This program prints "Programming is like building a multilingual puzzle"
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
