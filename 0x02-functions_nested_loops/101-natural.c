9daabd169f992d523b6a6c2b02abb1163e8adc18
Mohammed <ouarrdi@gmail.com>
2023-07-18 17:44:41 -0700

    13. Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: program that prints the first 50 Fibonacci
+ *		numbers, starting with 1 and 2, followed by a new line.
+ *
+ * Return 0 always
+ */
+
+int main(void)
+{
+	int sum = 0;
+	int n;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+			sum += n;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
