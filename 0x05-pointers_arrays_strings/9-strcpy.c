bbd8a9a989da9d2bffd8f58bd2986fa214a0b405
Mohammed <ouarrdi@gmail.com>
2023-07-24 18:17:25 -0700

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string.
+ *
+ * @s: control input
+ *
+ * Return: length of a string.
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+		i++;
+
+	return (i);
+}
+
+
+/**
+ * _strcpy - function that prints a string,
+ *		in reverse, followed by a new line.
+ *
+ * @dest: control input
+ * @src: control input
+ *
+ * Return: char *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i < _strlen(s); i++)
+		*(dest + i) = *(src + i);
+	return (dest);
+}
