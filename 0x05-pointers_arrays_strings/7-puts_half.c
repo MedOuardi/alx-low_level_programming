b688ed79000ccabeb0280a358b4d097b1ee66660
Mohammed <ouarrdi@gmail.com>
2023-07-24 17:53:29 -0700

    7. Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string.
+ *
+ * @s: control input
+ *
+ * Return: length of a string.
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+		i++;
+
+	return (i);
+}
+
+
+/**
+ * puts_half - function that prints a string,
+ *		in reverse, followed by a new line.
+ *
+ * @s: control input
+ */
+
+void puts_half(char *s)
+{
+	int i, length = _strlen(s);
+
+	for (i = length / 2; i < length; i++)
+		_putchar(*(s + i));
+	_putchar('\n');
+}
