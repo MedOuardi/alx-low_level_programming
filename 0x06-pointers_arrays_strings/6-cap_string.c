38437d9daa63347a60f5a690e981303ec70fcae3
Mohammed <ouarrdi@gmail.com>
2023-07-27 17:33:31 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * is_separator - verify if char is seperator
+ *
+ * @c: control input
+ *
+ * Return: 1 if seperator 0 otherwise
+ */
+
+int is_separator(char c)
+{
+	char separators[] = " \t\n,;.!?\"(){}\0";
+	int i = 0;
+
+	while (i <= 12)
+	{
+		if c == separators[i]
+			return (0);
+		i++;
+	}
+	return (1);
+}
+/**
+ * is_lower - functio that verify is char in lowercase
+ *
+ * @c: control input
+ *
+ * Return: 1 if lower 0 otherwhise
+ */
+
+int is_lower(char c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
+/**
+ * cap_string -  function that capitalizes all words of a string.
+ *
+ * @str: control input
+ *
+ * Return: String
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (is_seperator(str[i]))
+		{
+			if (is_lower(str[i + 1]))
+			{
+				i++;
+				str[i] -= 32;
+			}
+		}
+	}
+	return (str);
+
+}
